446b91a001a19e89aaee6e9c16c2cc2b3fcdb813
mchaitanya <chaitanyaaa@gmail.com>
2020-07-31 17:29:10 -0500

    Open and close the modal on click events

--- shared.js
+++ shared.js
@@ -1,3 +1,22 @@
 var backdrop = document.querySelector('.backdrop');
+var modal = document.querySelector('.modal');
+var closeButton = document.querySelector('.modal__action--negative');
 
-backdrop.style.display = 'block';
+var selectPlanButtons = document.querySelectorAll('.plan button');
+for (let button of selectPlanButtons) {
+    button.addEventListener('click', openModal);
+}
+
+backdrop.addEventListener('click', closeModal);
+closeButton.addEventListener('click', closeModal);
+
+function openModal() {
+    backdrop.style.display = 'block';
+    modal.style.display = 'block';
+}
+
+function closeModal() {
+    modal.style.display = 'none';
+    backdrop.style.display = 'none';
+}
+
