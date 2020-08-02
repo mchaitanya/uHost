d023176f04e896914fa8cebcb930b0aa9f08c480
mchaitanya <chaitanyaaa@gmail.com>
2020-08-01 19:04:45 -0500

    Add mobile nav & toggle button to all pages

--- shared.js
+++ shared.js
@@ -1,8 +1,12 @@
 var backdrop = document.querySelector('.backdrop');
-var modal = document.querySelector('.modal');
-var modalCloseButton = document.querySelector('.modal__action--negative');
 var mobileNav = document.querySelector('.mobile-nav');
 var toggleButton = document.querySelector('.toggle-button');
+
+var modal = document.querySelector('.modal');
+var modalCloseButton = document.querySelector('.modal__action--negative');
+if (modalCloseButton) {
+    modalCloseButton.addEventListener('click', closeModal);
+}
 
 var selectPlanButtons = document.querySelectorAll('.plan button');
 for (let button of selectPlanButtons) {
@@ -10,24 +14,27 @@ for (let button of selectPlanButtons) {
 }
 
 toggleButton.addEventListener('click', openSideNav);
-modalCloseButton.addEventListener('click', closeModal);
 backdrop.addEventListener('click', function() {
     closeModal();
     closeSideNav();
 });
 
 function openModal() {
-    // backdrop.style.display = 'block';
-    // modal.style.display = 'block';
-    backdrop.classList.add('open');
-    modal.classList.add('open');
+    if (modal) {
+        // backdrop.style.display = 'block';
+        // modal.style.display = 'block';
+        backdrop.classList.add('open');
+        modal.classList.add('open');
+    }
 }
 
 function closeModal() {
-    // modal.style.display = 'none';
-    // backdrop.style.display = 'none';
-    modal.classList.remove('open');
-    backdrop.classList.remove('open');
+    if (modal) {
+        // modal.style.display = 'none';
+        // backdrop.style.display = 'none';
+        modal.classList.remove('open');
+        backdrop.classList.remove('open');
+    }
 }
 
 function openSideNav() {
